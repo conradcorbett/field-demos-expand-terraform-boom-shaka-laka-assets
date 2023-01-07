c7395cd467e5f7281a7e1112baf7d6f927261e0b
conradcorbett <22246755+conradcorbett@users.noreply.github.com>
2023-01-06 17:03:20 -0800

    Update ssh.tf

--- gitclones/hashicups-application/ssh.tf
+++ gitclones/hashicups-application/ssh.tf
@@ -11,12 +11,13 @@ resource "tls_private_key" "awskey" {
   algorithm = "RSA"
 }
 
-resource "null_resource" "awskey" {
-  provisioner "local-exec" {
-    command = "echo \"${tls_private_key.awskey.private_key_pem}\" > ~/.ssh/awskey.pem"
-  }
 
-  provisioner "local-exec" {
-    command = "chmod 600 ~/.ssh/awskey.pem"
-  }
-}
+#resource "null_resource" "awskey" {
+#  provisioner "local-exec" {
+#    command = "echo \"${tls_private_key.awskey.private_key_pem}\" > ~/.ssh/awskey.pem"
+#  }
+#
+#  provisioner "local-exec" {
+#    command = "chmod 600 ~/.ssh/awskey.pem"
+#  }
+#}
